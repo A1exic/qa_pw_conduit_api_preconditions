40f893e4db1ca38c94de0d349639d89a515c3378
Alex <litvinovalex05@gmail.com>
2026-03-11 20:57:21 +0100

    b

--- tests/articles/viewArticle/viewArticleAsNotRegisteredUser.spec.js
+++ tests/articles/viewArticle/viewArticleAsNotRegisteredUser.spec.js
@@ -1,24 +1,30 @@
 import { test } from '../../_fixtures/fixtures';
 import { ExternalViewArticlePage } from '../../../src/ui/pages/article/view/ExternalViewArticlePage';
-import { createArticle } from '../../../src/ui/actions/articles/createArticle';
-import { signUpUser } from '../../../src/ui/actions/auth/signUpUser';
 
 test.use({ contextsNumber: 2, usersNumber: 1 });
 
-test.beforeEach(async ({ pages, users, articleWithoutTags }) => {
-  await signUpUser(pages[0], users[0], 1);
-  await createArticle(pages[0], articleWithoutTags, 1);
-});
+test.beforeEach(
+  async ({ registeredUsers, articlesApi, articleWithoutTags }) => {
+    const response = await articlesApi.createArticle(
+      articleWithoutTags,
+      registeredUsers[0].token,
+    );
+    const slug = await articlesApi.parseSlugFromResponse(response);
+    articleWithoutTags.url = slug;
+  },
+);
 
 test('View an article as not registered user', async ({
   articleWithoutTags,
   pages,
-  users,
+  registeredUsers,
 }) => {
   const page = new ExternalViewArticlePage(pages[1], 2);
 
   await page.open(articleWithoutTags.url);
   await page.articleHeader.assertTitleIsVisible(articleWithoutTags.title);
   await page.articleContent.assertArticleTextIsVisible(articleWithoutTags.text);
-  await page.articleHeader.assertAuthorNameIsVisible(users[0].username);
+  await page.articleHeader.assertAuthorNameIsVisible(
+    registeredUsers[0].username,
+  );
 });
--- tests/articles/viewArticle/viewArticleCreatedByAnotherUser.spec.js
+++ tests/articles/viewArticle/viewArticleCreatedByAnotherUser.spec.js
@@ -1,25 +1,30 @@
 import { test } from '../../_fixtures/fixtures';
 import { InternalViewArticlePage } from '../../../src/ui/pages/article/view/InternalViewArticlePage';
-import { createArticle } from '../../../src/ui/actions/articles/createArticle';
-import { signUpUser } from '../../../src/ui/actions/auth/signUpUser';
 
 test.use({ contextsNumber: 2, usersNumber: 2 });
 
-test.beforeEach(async ({ pages, users, articleWithoutTags }) => {
-  await signUpUser(pages[0], users[0], 1);
-  await signUpUser(pages[1], users[1], 2);
-  await createArticle(pages[0], articleWithoutTags, 1);
-});
+test.beforeEach(
+  async ({ registeredUsers, articlesApi, articleWithoutTags }) => {
+    const response = await articlesApi.createArticle(
+      articleWithoutTags,
+      registeredUsers[0].token,
+    );
+    const slug = await articlesApi.parseSlugFromResponse(response);
+    articleWithoutTags.url = slug;
+  },
+);
 
 test('View an article created by another registered user', async ({
   articleWithoutTags,
   pages,
-  users,
+  registeredUsers,
 }) => {
   const page = new InternalViewArticlePage(pages[1], 2);
 
   await page.open(articleWithoutTags.url);
   await page.articleHeader.assertTitleIsVisible(articleWithoutTags.title);
   await page.articleContent.assertArticleTextIsVisible(articleWithoutTags.text);
-  await page.articleHeader.assertAuthorNameIsVisible(users[0].username);
+  await page.articleHeader.assertAuthorNameIsVisible(
+    registeredUsers[0].username,
+  );
 });
